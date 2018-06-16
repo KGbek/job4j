69f72f350382a78c56cde576d57d9b13c0b7e0fa
Manabaev Edilbek <edilbek@gmail.com>
2018-06-16 08:22:11 +0300

    Выполнения задания конвертации валюты.

---
+++ chapter_001/src/main/java/ru/job4j/convertor/Convertor.java
@@ -0,0 +1,28 @@
+package ru.job4j.convertor;
+
+/**
+ * Конвертор валют.
+ */
+
+public class Convertor {
+
+    /**
+     * Конвертируем доллар на рубли.
+     * @param value рубли.
+     * @return доллар.
+     */
+
+    public int rubleToDollar(int value) {
+        return value / 63;
+    }
+
+    /**
+     * Конертируем евро на рубли.
+     * @param value рубли.
+     * @return евро.
+     */
+
+    public int rubleToEuro(int value){
+        return value / 74;
+    }
+}
