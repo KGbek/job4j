f34b87d45c87e20aa961cd7e3d01c0d1ae2f5503
Manabaev Edilbek <edilbek@gmail.com>
2018-06-16 08:50:15 +0300

    Выполнено задания нахождения идеального веса для мужчин и женщин.

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,33 @@
+package ru.job4j.calculator;
+
+/**
+ * Программа расчета идеального веса.
+ *
+ * Формула:
+ * Идеальный вес для мужчин = (рост в сантиметрах – 100) · 1,15.
+ * Идеальный вес для женщин = (рост в сантиметрах – 110) · 1,15.
+ */
+public class Fit {
+
+    /**
+     * Идеальный вес для мужщины.
+     * @param height Рост.
+     * @return идеальный вес.
+     */
+    double result;
+
+    public double manWeight(double height) {
+        result = (height - 100) * 1.15;
+        return result;
+    }
+
+    /**
+     * Идеальный вес для женщины.
+     * @param height Рост.
+     * @return идеальный вес.
+     */
+    public double womanWeight(double height) {
+        result = (height - 110) * 1.15;
+        return result;
+    }
+}
