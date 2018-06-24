514c42adb0526f6c3f634320c0d3d6d324da3a0e
Manabaev Edilbek <edilbek@gmail.com>
2018-06-24 11:44:28 +0300

    Правильный подсчет сумму четных чисел

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -1,17 +1,17 @@
 package ru.job4j.loop;
 
 public class Counter {
+    int result = 0;
 
     public int add(int start, int finish){
-        int summ = 0;
 
         for(start = 0; start<=10; start++){
             if(start % 2 == 0){
-                summ = finish + start;
+                result = result + start;
 
             }
         }
-        return summ;
+        return result;
 
     }
 
