ca0af029b7d2c993de0de67eb6f82e930866544e
Manabaev Edilbek <edilbek@gmail.com>
2018-06-27 22:07:12 +0300

    After with some changing

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,26 @@
+package ru.job4j.calculator;
+
+
+public class Calculator {
+    double result;
+
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+
+    public double getResult() {
+        return this.result;
+    }
+
+    public void substract(double first, double second) {
+        this.result = first - second;
+    }
+
+    public void div(double first, double second) {
+        this.result = first / second;
+    }
+
+    public void multiple(double first, double second) {
+        this.result = first * second;
+    }
+}
