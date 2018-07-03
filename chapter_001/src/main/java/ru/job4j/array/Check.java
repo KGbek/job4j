567cc8a5ef0705554f4ac1daba44cd5e6a49d16e
Manabaev Edilbek <edilbek@gmail.com>
2018-07-03 14:18:12 +0300

    Поправленный метод

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -3,14 +3,9 @@ package ru.job4j.array;
 public class Check {
     public boolean mono(boolean[] data) {
         boolean result = false;
-        for (int index = 0; index != data.length; index++) {
-            for (int index2 = 0; index2 != data.length; index2++) {
-                if (index == index2) continue;
-                if (data[index] == data[index2]){
-                    return true;
-                } else return false;
-            }
-        }
-        return result;
+        if(data.length > 1)
+            return data[0] == data[1];
+        else
+            return false;
     }
 }
