4e14fb4e085f254438bf1c04e62b1e7055770620
Manabaev Edilbek <edilbek@gmail.com>
2018-07-04 13:28:58 +0300

    Поправка

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -2,14 +2,21 @@ package ru.job4j.array;
 
 public class Check {
     public boolean mono(boolean[] data) {
-        boolean result = false;
-        for (int i = 0; i != data.length; i++) {
-            result = data[0] == data[i];
+//        boolean result = false;
+//        for (int i = 0; i != data.length; i++) {
+//            result = data[0] == data[i];
+//            if (data[0] != data[i]) {
+//               result = false;
+//               return result;
+//            }
+//
+//        }
+//        return result;
+        boolean result = true;
+        for (int i =1; i < data.length; i++) {
             if (data[0] != data[i]) {
-               result = false;
-               return result;
+                result = false;
             }
-
         }
         return result;
     }
