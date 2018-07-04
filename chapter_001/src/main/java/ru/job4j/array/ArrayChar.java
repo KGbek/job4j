8a77b6942a80fc81655865484a77df4ec5005179
Manabaev Edilbek <edilbek@gmail.com>
2018-07-04 13:05:46 +0300

    Метод который находит слова начинается с ...

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    private char[] data;
+
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+    /**
+     * Проверяет. что слово начинается с префикса.
+     * @param prefix префикс.
+     * @return если слово начинаеться с префикса
+     */
+
+    public boolean startWith(String prefix) {
+        boolean result = true;
+        char[] value = prefix.toCharArray();
+        // проверить. что массив data имеет первые элементы одинаковые с value
+        int minLength = Math.min(data.length, value.length);
+        for (int i = 0; i < minLength; i++) {
+            if (data[i] != value[i]) {
+                result = false;
+                return result;
+            }
+        }
+        return result;
+    }
+}
