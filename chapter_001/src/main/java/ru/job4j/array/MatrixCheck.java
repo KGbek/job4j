13de1f0396a3525c9aad78ab21916528035ff2c5
Manabaev Edilbek <edilbek@gmail.com>
2018-07-14 00:58:05 +0300

    Проверка массива по диагоналам на значение true либо false

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -5,9 +5,11 @@ public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
         for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data.length; j++) {
-                if (data[0][0] != data[i][j]) {
-                    result = false;
+            for (int j = 0; j < data[i].length; j++) {
+                if ((i == j) || (i + j) == (data.length - 1)) {
+                    result = data[i][j];
+                } else if (result == false) {
+                    return false;
                 }
             }
         }
--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -3,11 +3,11 @@ package ru.job4j.array;
 public class Square {
     public int[] calculate(int bound) {
         int[] rst = new int[bound];
-        for (int i = 0; i != rst.length; i++) {
-            rst[0] = 1;
-            rst[1] = 2;
-            rst[2] = 3;
-            rst[i] *= rst[i];
+        for (int i = 0; i < bound; i++) {
+            for (int j = 1; j < rst.length; j++) {
+                return rst;
+            }
+            return rst;
         }
         return rst;
     }
