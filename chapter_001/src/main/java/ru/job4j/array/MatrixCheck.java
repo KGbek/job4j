c965498a88221842c2079e694842611f987b977b
Manabaev Edilbek <edilbek@gmail.com>
2018-07-14 10:10:18 +0300

    Проверка двумерного массива одним циклом

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -5,12 +5,12 @@ public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
         for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[i].length; j++) {
-                if ((i == j) || (i + j) == (data.length - 1)) {
-                    result = data[i][j];
-                } else if (result == false) {
-                    return false;
-                }
+            if (data[0][0] != data[i][i]) {
+                result = false;
+                break;
+            } else if (data[data.length - 1][0] != data[data.length - 1 - i][i]) {
+                result = false;
+                break;
             }
         }
         return result;
